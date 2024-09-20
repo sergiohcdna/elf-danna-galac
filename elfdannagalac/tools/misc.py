b8affe12634001303281b66734f8c3849f7404ef
sergiohcdna <skerzot@ciencias.unam.mx>
2024-09-20 01:53:27 -0400

    Add function to print elapsed time for a process or function

--- elfdannagalac/tools/misc.py
+++ elfdannagalac/tools/misc.py
@@ -8,6 +8,8 @@
 
 import numpy as np
 
+import time
+
 class GenericDescriptor:
 
     def __init__(self,getter,setter):
@@ -101,40 +103,15 @@ def ValidValue(attr_name,min_val=None,max_val=None):
 
     return decorator
 
-# def ValidnpArray( attr_name , min_val=None , max_val=None ) :
+def elapsed_time(start,msg=''):
 
-#     def decorator( cls ) :
+    e       = int(time.time() - start)
+    strtime = '{:02d}:{:02d}:{:02d}'.format(e//3600, (e%3600//60), e%60)
+    if len(msg) > 0:
+        msg = msg + strtime
+    else:
+        msg = f'Time to finish whatever process you are doing was: {strtime}'
 
-#         name = "__" + attr_name
+    print(msg)
 
-#         def getter( self ) :
-
-#             return getattr( self , name )
-
-#         def setter( self , value ) :
-
-#             msg = '\n\t{0} must be a numpy ndarray'.format( attr_name )
-
-#             assert isinstance( value , np.ndarray ) , msg
-
-#             if min_val is not None and ( value < min_val ).all() :
-
-#                 msg = ( '\n\t{0} has some value that  '.format( attr_name ) +
-#                     'is below the minimum value allowed: {0}'.format( min_val ) )
-
-#                 raise ValueError( msg )
-
-#             if max_val is not None and ( value > max_val ).all() :
-
-#                 msg = ( '\n\t{0} has some value that '.format( attr_name ) +
-#                     'is above the maximum value allowed: {0}'.format( min_val ) )
-
-#                 raise ValueError( msg )
-
-#             setattr( self , name , value )
-
-#         setattr( cls , attr_name , GenericDescriptor( getter , setter ) )
-
-#         return cls
-
-#     return decorator
+    return
