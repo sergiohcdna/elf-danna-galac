f35be2397ae185c320b906f52c7f9d226b204aa6
sergiohcdna <skerzot@ciencias.unam.mx>
2026-01-29 01:07:34 -0500

    Adding some erros related to setting a DM halo

---
+++ elfdannagalac/tools/customerrors.py
@@ -0,0 +1,23 @@
+###############################################################################
+# Diffusion of electrons  in the intraclluster medium of galaxy clusters      #
+#   - Custom Error classes to use when configuring halos                      #
+#-----------------------------------------------------------------------------#
+#                      THE ELF-DANNA-GALAC Task force                         #
+#                      - Arlette Melo Galindo                                 #
+#                      - Miguel A. Sánchez Conde                              #
+#                      - Sergio Hernández Cadena                              #
+#-----------------------------------------------------------------------------#
+#             January-2026                                                    #
+###############################################################################
+
+class SubHaloMassError(Exception):
+
+    pass
+
+class DMProfileError(Exception):
+
+    pass
+
+class DMConcentrationError(Exception):
+
+    pass
