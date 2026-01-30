2b06fe6189a770b59e307dda4e7d1ecc74964d44
sergiohcdna <skerzot@ciencias.unam.mx>
2026-01-30 01:22:41 -0500

    Add general imports

---
+++ elfdannagalac/substructure/__init__.py
@@ -0,0 +1,4 @@
+from .subhalo_dndc import p_nsub_c,p_nsub_c_int
+from .subhalo_dndm import p_nsub_m,p_nsub_m_int
+from .subhalo_dndv import p_nsub_v,p_nsub_v_int
+from .subhalos import p_nsub_tot,nsub_r,nsub_tot,msub_tot
