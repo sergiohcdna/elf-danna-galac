8ccb2f8d08f4069ede59ffee1b809e39dfd9bbff
sergiohcdna <skerzot@ciencias.unam.mx>
2026-02-20 01:37:22 -0500

    Add function to estimate luminosity:  = 4\pi\intdr r^2\rho$. Remove function to estimate spherical d factor

--- elfdannagalac/astrofactors/dfactor.py
+++ elfdannagalac/astrofactors/dfactor.py
@@ -11,40 +11,40 @@
 ###############################################################################
 
 import astropy.units as u
-import numpy as np
 
-from scipy.integrate import quad
-
-from ..dmsrc.dmsource import NFW_profile
-
-from ..tools.conversions import convert_density
+from ..dmsrc.dmsource import get_enclosed_mass_nfw
 
 
-def dfactor_on_sphere_nfw(
+def luminosity_decay_nfw(
     rmax   : u.Quantity,
     rs     : u.Quantity,
     rhos   : u.Quantity,
     rsat   : u.Quantity,
-    rhosat : u.Quantity
+    rhosat : u.Quantity,
+    rtrunc : u.Quantity,
 ) -> u.Quantity:
 
     """
-    Astrophysical D factor computed for the NFW 
+    Decay luminosity computed for the NFW 
     DM profile. The integral is done without 
     considering the line-of-sigh projection. 
     We use the units of the scale radius to make 
     comparisons between the different radial distances. 
     The default units for mass and density are 
     $M_\odot$ and $M_odot~[\text{distance}]^{3}$. 
-    The D factor is:
+    The luminosity is:
 
-    $D = 4\pi\int_0^{r_\text{max}} {\rm d}r \rho(r)$
+    $D = 4\pi\int_0^{r_\text{max}} {\rm d}r r^2 \rho(r)$
+
+    That is, the emissivity and the luminosity for decay 
+    are proportional to the enclosed mass up to radius r_max.
 
     The integral is always done from the center of the host 
     halo to a maximum radius rmax. This should be enough for 
     all the purposes of this code. Please note, that we include 
-    the angular factor $4\pi$. The units of the D factor are 
-    $M_odot~[\text{distance}]^{2}$
+    the angular factor $4\pi$ and the units of the emissivity are 
+    $M_odot$. To get the actual luminosty, a factor of the 
+    decay rate of DM needs to be included.
     
     :param rmax: Upper limit in radius for the integral
     :type rmax: u.Quantity
@@ -60,39 +60,8 @@ def dfactor_on_sphere_nfw(
     :rtype: Quantity
     """
 
-    lunit = rs.unit
-    dunit = u.Msun/lunit**3
-    units = dunit*(lunit)
+    mass = get_enclosed_mass_nfw(
+        rmax,rs,rhos,rsat,rhosat,rtrunc,length_unit=rs.unit
+    )
 
-    rhos_   = convert_density(rhos,new_unit=dunit)
-    rhosat_ = convert_density(rhosat,new_unit=dunit)
-
-
-    def integrand(r,rs,rhos,rsat,rhosat):
-
-        return NFW_profile(
-            r*lunit,rs,rhos,rsat,rhosat,length_unit=lunit
-        ).value
-
-    dfactor_01 = quad(
-        integrand,
-        0,
-        rsat.to(lunit).value,
-        args=(rs,rhos_,rsat,rhosat_)
-    )[0]
-
-    dfactor_02 = quad(
-        integrand,
-        rsat.to(lunit).value,
-        rs.to(lunit).value,
-        args=(rs,rhos,rsat,rhosat)
-    )[0]
-
-    dfactor_03 = quad(
-        integrand,
-        rs.to(lunit).value,
-        rmax.to(lunit).value,
-        args=(rs,rhos,rsat,rhosat)
-    )[0]
-
-    return 4*np.pi*(dfactor_01+dfactor_02+dfactor_03)*units
+    return mass
