adc2666cb26313384e5d6f6f3a413d2a705b9b4c
sergiohcdna <skerzot@ciencias.unam.mx>
2026-04-07 15:44:02 -0400

    Update global imports

--- elfdannagalac/substructure/__init__.py
+++ elfdannagalac/substructure/__init__.py
@@ -1,4 +1,6 @@
+from .population import SubHaloPopulation
+from .samplers import dndm_PL,dndvCoredNFW
 from .subhalo_dndc import p_nsub_c,p_nsub_c_int
 from .subhalo_dndm import p_nsub_m,p_nsub_m_int
 from .subhalo_dndv import p_nsub_v,p_nsub_v_int
-from .subhalos import p_nsub_tot,nsub_r,nsub_tot,msub_tot
+from .subhalos import p_nsub_tot,nsub_r,nsub_tot,msub_tot,rhosub
