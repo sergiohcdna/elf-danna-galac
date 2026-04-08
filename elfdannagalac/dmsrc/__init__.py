5e5be079e5116261034d0152a0bcd5189c3383c1
sergiohcdna <skerzot@ciencias.unam.mx>
2026-04-08 07:48:12 -0400

    Updating global imports

--- elfdannagalac/dmsrc/__init__.py
+++ elfdannagalac/dmsrc/__init__.py
@@ -2,14 +2,19 @@ from .dmsource import allowed_profiles,allowed_spatial_types
 from .dmsource import (
     NFW_profile,
     dm_mass_density,
-    SmoothDMHaloMassDensityGrid,
-    SmoothDMHaloMassSquaredDensityGrid,
-    preparePointLikeDMSource,
-    prepareSmoothExtendedDMSource,
     get_rhosat,
     get_enclosed_mass_nfw
 )
 
 from .concentrations import allowed_concentrations
 from .concentrations import get_c,get_c_sub
+
+from .grids import (
+    SmoothDMHaloMassDensityGrid,
+    SmoothDMHaloMassSquaredDensityGrid,
+    preparePointLikeDMSource,
+    prepareSmoothExtendedDMSource,
+)
+
 from .halo import DMHalo
+from .smooth import get_smooth_dm_density
